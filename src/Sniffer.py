f611d7be14eec6155ebf3f3106507ba20b0eb8d8
Kevin Brady <kgb@nist.gov>
2024-09-05 20:00:08 +0000

    Update Sniffer.py

--- src/Sniffer.py
+++ src/Sniffer.py
@@ -1,8 +1,10 @@
+import keras.src.ops
 import psutil
 import tensorflow_decision_forests as tfdf
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+from collections import OrderedDict, deque
 
 from scapy.all import *
 from scapy.layers.inet import IP, TCP, UDP
@@ -12,19 +14,20 @@ from src.FlowMeterMetrics import FlowMeterMetrics
 from src.metadata.input_layer import InputLayer
 from src.utils import pretty_time_delta, normalize
 
-#MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/neural_network/deep_neural_network/DeepNeuralNet'
-#MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/neural_network/logistic_regression/LogisticRegression'
-MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/decision_tree/random_forest/RandomForestModel'
+MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/neural_network/deep_neural_network/DeepNeuralNet'
+
+
+# MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/neural_network/logistic_regression/LogisticRegression'
+# MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/decision_tree/random_forest/RandomForestModel'
 # MODEL_FILEPATH = '/home/kgb/PycharmProjects/TensorFlowPcap/src/models/decision_tree/boosted_tree/BoostedTreesModel'
 
 
 class Sniffer:
     counter = PacketCounter()
     flow_meter = FlowMeterMetrics(output_mode="flow")
-    packet_data = []
-    model = tf.keras.models.load_model(MODEL_FILEPATH)
+    model = tf.keras.layers.TFSMLayer(MODEL_FILEPATH, call_endpoint="serving_default")
     input_layer = InputLayer()
-    flow_metrics = pd.DataFrame(columns=input_layer.feature_columns)
+    packet_queue = deque(maxlen=200)
 
     def __init__(self, interfaces=None):
 
@@ -69,19 +72,21 @@ class Sniffer:
             self.counter.packet_count_preprocessed += 1
 
             flow, direction = self.flow_meter.process_packet(pkt)
-            latest_flow_metrics = flow.get_data(pkt, direction)
+            packet_data = flow.get_data(pkt, direction)
+            self.packet_queue.appendleft(
+                {x: y for x, y in packet_data.items() if x not in self.input_layer.exclude_features}
+            )
 
-            flow_metrics = pd.DataFrame.from_dict({key: [value] for key, value in latest_flow_metrics.items() if
-                                                   key not in self.input_layer.exclude_features})
-            self.flow_metrics = pd.concat([self.flow_metrics, flow_metrics])
-
+            print(len(self.packet_queue))
             if self.counter.get_packet_count_preprocessed() > 5:
+                pass
+                # input = keras.layers.Concatenate(axis=0)(self.packet_queue[len(self.packet_queue) - 1].values())
 
-                input = self.flow_metrics.to_numpy()
-                #input = np.apply_along_axis(func1d=normalize, axis=0, arr=input, method='zscore')
-                input = [np.split(row, self.flow_metrics.shape[1]) for row in input]
+                # input = self.flow_metrics.to_numpy()
+                # if MODEL_FILEPATH.__contains__('neural_network'):
+                # input = np.apply_along_axis(func1d=normalize, axis=0, arr=input, method='zscore')
+                # input = [np.split(row, self.flow_metrics.shape[1]) for row in input]
 
-                print(self.model.predict(input[-1]))
+                input = self.packet_queue[len(self.packet_queue) - 1]
 
-            if self.flow_metrics.size > 200:
-                self.flow_metrics.drop(0)
+                print(self.model(input))
